1f75bb1c103f5e7e3bca9272cce65357bebe012e
Tim <firstpesus43@gmail.com>
2025-10-25 13:39:26 +0000

    add initialize ix, pool config. add deposit liquidity ix

---
+++ anchor-amm-starter-q4-25/programs/anchor-amm-starter-q4-25/src/instructions/deposit.rs
@@ -0,0 +1,143 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{mint_to, transfer, Mint, MintTo, Token, TokenAccount, Transfer},
+};
+use constant_product_curve::{ConstantProduct, XYAmounts};
+
+use crate::{error::AmmError, state::Config};
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub mint_x: Account<'info, Mint>,
+    pub mint_y: Account<'info, Mint>,
+    #[account(
+        mut,
+        seeds = [b"config", config.seed.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub config: Account<'info, Config>,
+    #[account(
+        mut,
+        seeds = [b"lp", config.key().as_ref()],
+        mint::decimals = 6,
+        mint::authority = config,
+        bump = config.config_bump,
+    )]
+    pub mint_lp: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = config,
+    )]
+    pub vault_x: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = config,
+    )]
+    pub vault_y: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = config,
+    )]
+    pub user_x: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = config,
+    )]
+    pub user_y: Account<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        payer = signer,
+        associated_token::mint = mint_lp,
+        associated_token::authority = config,
+    )]
+    pub user_lp: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Deposit<'info> {
+    pub fn deposit(&mut self, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        require!(self.config.locked == false, AmmError::PoolLocked);
+        
+        let (x, y) = match self.mint_lp.supply == 0
+            && self.vault_x.amount == 0
+            && self.vault_y.amount == 0
+        {
+            true => (max_x, max_y),
+            false => {
+                let amounts: XYAmounts = ConstantProduct::xy_deposit_amounts_from_l(
+                    self.vault_x.amount,
+                    self.vault_y.amount,
+                    self.mint_lp.supply,
+                    amount,
+                    6
+                )
+                .unwrap();
+                (amounts.x, amounts.y)
+            }
+        };
+
+        self.deposit_tokens(true, x)?;
+        self.deposit_tokens(false, y)?;
+
+        self.mint_lp_tokens(amount)?;
+
+        Ok(())
+    }
+
+    pub fn deposit_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
+        let (from, to) = match is_x {
+            true => (
+                self.user_x.to_account_info(),
+                self.vault_x.to_account_info(),
+            ),
+            false => (
+                self.user_y.to_account_info(),
+                self.vault_y.to_account_info(),
+            )
+        };
+
+        let transfer_ctx = CpiContext::new(
+            self.token_program.to_account_info(),
+            Transfer {
+                from,
+                to,
+                authority: self.signer.to_account_info()
+            },
+        );
+
+        transfer(transfer_ctx, amount)
+    }
+
+    pub fn mint_lp_tokens(&mut self, amount: u64) -> Result<()> {
+        let signer_seeds: [&[&[u8]]; 1] = [&[
+            b"lp",
+            self.config.to_account_info().key.as_ref(),
+            &[self.config.config_bump],
+        ]];
+
+        let mint_ctx = CpiContext::new_with_signer(
+            self.mint_lp.to_account_info(),
+            MintTo {
+                mint: self.mint_lp.to_account_info(),
+                to: self.signer.to_account_info(),
+                authority: self.config.to_account_info()
+            },
+            &signer_seeds
+        );
+
+        mint_to(mint_ctx, amount)
+    }
+}
--- anchor-amm-starter-q4-25/programs/anchor-amm-starter-q4-25/src/lib.rs
+++ anchor-amm-starter-q4-25/programs/anchor-amm-starter-q4-25/src/lib.rs
@@ -1,16 +1,22 @@
 use anchor_lang::prelude::*;
 
+pub mod state;
+pub mod error;
+pub mod instructions;
+
+pub use instructions::*;
+
 declare_id!("CR6WzBT3A9QA3siLvvT82C6EzqbpnYYCHzMCJkKjw4RS");
 
 #[program]
 pub mod anchor_amm_starter_q4_25 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize(ctx: Context<Initialize>, seed: u64, fee: u16, authority: Option<Pubkey>) -> Result<()> {
+        ctx.accounts.init(seed, fee, authority, ctx.bumps)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
