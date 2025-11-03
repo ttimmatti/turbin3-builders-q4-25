6327705c84f0260bb8aa3284568d3537f73dae77
Tim <firstpesus43@gmail.com>
2025-11-03 20:50:14 +0000

    add session key program

--- private-payments-demo/run-mb-val.sh
+++ private-payments-demo/run-mb-val.sh
@@ -2,4 +2,5 @@ anchor build && \
 mb-test-validator \
  --reset \
  --bpf-program BTWAqWNBmF2TboMh3fxMJfgR16xGHYD7Kgr2dPwbRPBi scripts/local-dumps/BTWAqWNBmF2TboMh3fxMJfgR16xGHYD7Kgr2dPwbRPBi.so \
+ --bpf-program KeyspM2ssCJbqUhQ4k7sveSiY4WjnYsrXkC8oDbwde5 scripts/local-dumps/KeyspM2ssCJbqUhQ4k7sveSiY4WjnYsrXkC8oDbwde5.so \
  --bpf-program jGhY7xAfrngebRtyQ5xbNghM3rzVj84GQxZamsZ4ngD target/deploy/private_payments.so
